bff3f9ae9ad7523ec029649ff0b42dfcd524da15
Ageildo Vitorino <107153811+Ageildo@users.noreply.github.com>
2024-05-16 18:00:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ function sortear(){
   let sorteados = [];
     let numero;
     
-    for (let i = 0; i < quantidade; i++) {
-        numero = obterNumeroAleatorio(de, ate);
-        sorteados.push(numero);
-    }
-    
-    alert(sorteados);
+  for (let i = 0; i < quantidade; i++) {
+    numero = obterNumeroAleatorio(de, ate);
+  while (sorteados.includes(numero)) {
+    numero = obterNumeroAleatorio(de, ate);
+  }
+  sorteados.push(numero); 
+}
+
+let resultado = document.getElementById('resultado')
+resultado.textContent = `Números sorteados:${sorteados.join(', ')}`;
+}
+
+function obterNumeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
